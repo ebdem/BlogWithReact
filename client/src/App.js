5b81a6007b272a64561d3b6f1a65bfb10432bc47
ebdem <ebu.bekir.3333@gmail.com>
2021-04-12 02:44:31 +0300

    Temel yapı kuruldu frontend kısmı için redux ve router

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,66 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import {
+  CssBaseline,
+  Container,
+  Grid,
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  IconButton
+} from '@material-ui/core'
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
+import PenIcon from '@material-ui/icons/Create'
+import PostList from './components/PostList'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginRight: theme.spacing(2)
+  },
+  title: {
+    flexGrow: 1
+  },
+  container: {
+    marginTop: theme.spacing(3)
+  }
+}))
+
 function App() {
+  const classes = useStyles()
   return (
-    <div>Hello from EBDEM</div>
-  );
+    <>
+      <CssBaseline />
+      <Container maxWidth="lg">
+        <AppBar position="static" color="inherit" elevation={0}>
+          <Toolbar>
+            <IconButton edge="start" className={classes.container} color="inherit" />
+            <Typography variant="h6" color="secondary" className={classes.title}>
+              <a style={{ textDecoration: 'none' }} href="http://localhost:3000/posts">
+                Uykusuz Blog
+              </a>
+            </Typography>
+            <Button color="primary" variant="outlined" startIcon={<PenIcon />}>
+              Yeni Yazı
+            </Button>
+          </Toolbar>
+        </AppBar>
+        <Grid container className={classes.container}>
+          <Grid xs={12} item>
+            <Router>
+              <Switch>
+                <Route exact path="/posts" component={PostList} />
+              </Switch>
+              <Redirect from="/" to="/posts" />
+            </Router>
+          </Grid>
+        </Grid>
+      </Container>
+    </>
+  )
 }
 
-export default App;
+export default App
